79abbfad055fede14176ba42f0bb573422893197
aiden <danyang.aiden@outlook.com>
2023-07-09 20:31:47 +0800

    chore: add vercel plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,13 @@
 
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/content'],
+
   content: {
     highlight: {
       theme: 'dracula',
     },
   },
+
   devtools: { enabled: true },
+  plugins: ['~/plugins/vercel.ts', { src: '~/plugins/vercel.ts', mode: 'client' }],
 })
